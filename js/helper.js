2140fb4b9f6b1a9c7c5d3c80781fbdc22e44e1b8
Sahil <sahilflash123@gmail.com>
2021-07-09 12:30:31 +0530

    started button functionality

--- js/helper.js
+++ js/helper.js
@@ -1,98 +1,84 @@
 
 //free space in memory
-var memory = [20,100,25,50,75,60,10,15];
-const initial_memory = [20,100,25,50,75,60,10,15];
+var memory = [20, 100, 25, 50, 75, 60, 10, 15];
+const initial_memory = [20, 100, 25, 50, 75, 60, 10, 15];
 
 //process size
-var process_size = [18,17,15,22,30];
+var process_size = [18, 17, 15, 22, 30];
 
 var last = 0;
 
 //first fit algo
-function firstFit(proc)
-{
-   for(var i=0; i < memory.length() ; i++)
-   {
-         if(proc<memory[i])
-         {
-           memory[i]=memory[i]-proc;
-           last=i;
-           break;
-         }
-   }
+function firstFit(proc) {
+  for (var i = 0; i < memory.length(); i++) {
+    if (proc < memory[i]) {
+      memory[i] = memory[i] - proc;
+      last = i;
+      break;
+    }
+  }
 
 
 }
 
 //next-fit
-function nextFit(proc)
-{
+function nextFit(proc) {
 
 
-   for(var i = last; i<memory.length() ; i++)
-   {
-     if(proc<memory[i])
-     {
-       memory[i]=memory[i]-proc;
-       last=i;
-       break;
-     }
-   }
+  for (var i = last; i < memory.length(); i++) {
+    if (proc < memory[i]) {
+      memory[i] = memory[i] - proc;
+      last = i;
+      break;
+    }
+  }
 
 
 
 }
 
 //best-fit
-function bestFit(proc)
-{
-     var index = 0;
-     //something very large
-     var diff = 999;
+function bestFit(proc) {
+  var index = 0;
+  //something very large
+  var diff = 999;
 
-    for(var i=0 ; i<memory.length() ; i++)
-    {
-        if(memory[i]>proc)
-        {
-             if(diff>memory[i]-proc)
-             {
-               diff=memory[i]-proc;
-               index=i;
+  for (var i = 0; i < memory.length(); i++) {
+    if (memory[i] > proc) {
+      if (diff > memory[i] - proc) {
+        diff = memory[i] - proc;
+        index = i;
 
-             }
-
-        }
+      }
 
     }
 
-   memory[index]=memory[index]-proc;
+  }
+
+  memory[index] = memory[index] - proc;
 
 
 }
 
 //best-fit
-function worstFit(proc)
-{
-     var index = 0;
-     //something very small
-     var diff = 0;
+function worstFit(proc) {
+  var index = 0;
+  //something very small
+  var diff = 0;
 
-    for(var i=0 ; i<memory.length() ; i++)
-    {
-        if(memory[i]>proc)
-        {
-             if(diff<memory[i]-proc)
-             {
-               diff=memory[i]-proc;
-               index=i;
+  for (var i = 0; i < memory.length(); i++) {
+    if (memory[i] > proc) {
+      if (diff < memory[i] - proc) {
+        diff = memory[i] - proc;
+        index = i;
 
-             }
-
-        }
+      }
 
     }
 
-   memory[index]=memory[index]-proc;
+  }
+
+  memory[index] = memory[index] - proc;
 
 
 }
@@ -102,18 +88,33 @@ function worstFit(proc)
 //html filling code
 
 //filling up the memory cells
-for(var i=1; i<9; ++i)
-{
-  document.getElementById(i).append(memory[i-1]);
+for (var i = 1; i < 9; ++i) {
+  document.getElementById(i).append(memory[i - 1]);
 }
 
 //filling up the filled cells space
-var temp=0;
-var string="";
-for(var i=1; i<9; ++i)
-{
-  string="";
-  temp=((initial_memory[i-1]-memory[i-1])/initial_memory[i-1])*100;
-  string=temp + string;
-  console.log(document.getElementById(i).childNodes[1].style.width=string);
+var temp = 0;
+var string = "";
+for (var i = 1; i < 9; ++i) {
+  string = "";
+  temp = ((initial_memory[i - 1] - memory[i - 1]) / initial_memory[i - 1]) * 100;
+  string = temp + string;
+  console.log(document.getElementById(i).childNodes[1].style.width = string);
 }
+
+//adding functionality to button
+function start(func) {
+
+  //filling up the memory cells
+  for (var i = 1; i < 9; ++i) {
+    document.getElementById(i).append(memory[i - 1]);
+  }
+
+  for(var i=0; i<8; ++i)
+  {
+    setTimeout(2000);
+      
+    func()
+  }
+}
+document.getElementById("next").addEventListener("click",)
